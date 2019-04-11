d736f1cb797854f14d2badf5eda8841913586a3f
Jordan Epps <jordanepps@gmail.com>
2019-04-11 14:42:36 -0400

    Finish adding all auth

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Header from '../Header/Header';
-// import PrivateRoute from '../Utils/PrivateRoute'
-// import PublicOnlyRoute from '../Utils/PublicOnlyRoute'
+import PrivateRoute from '../Utils/PrivateRoute';
+import PublicOnlyRoute from '../Utils/PublicOnlyRoute';
 import ThingListPage from '../../routes/ThingListPage/ThingListPage';
 import ThingPage from '../../routes/ThingPage/ThingPage';
 import LoginPage from '../../routes/LoginPage/LoginPage';
@@ -29,10 +29,10 @@ class App extends Component {
 						<p className="red">There was an error! Oh no!</p>
 					)}
 					<Switch>
-						<Route exact path={'/'} component={ThingListPage} />
-						<Route path={'/login'} component={LoginPage} />
-						<Route path={'/register'} component={RegistrationPage} />
-						<Route path={'/thing/:thingId'} component={ThingPage} />
+						<PrivateRoute exact path={'/'} component={ThingListPage} />
+						<PublicOnlyRoute path={'/login'} component={LoginPage} />
+						<PublicOnlyRoute path={'/register'} component={RegistrationPage} />
+						<PrivateRoute path={'/thing/:thingId'} component={ThingPage} />
 						<Route component={NotFoundPage} />
 					</Switch>
 				</main>
